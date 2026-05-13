997193fe930173c96f935f945a267dda7b31ceda
Easybranding <sogulaa@gmail.com>
2026-05-13 11:26:12 +0200

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,1 +1,29 @@
+function validateSAID(idNumber){
 
+if(idNumber.length !== 13){
+  return false;
+}
+
+return /^\d+$/.test(idNumber);
+
+}
+
+function validatePhone(phone){
+
+if(phone.length < 10){
+  return false;
+}
+
+return true;
+
+}
+
+function validateIncome(income){
+
+if(income <= 0){
+  return false;
+}
+
+return true;
+
+}
