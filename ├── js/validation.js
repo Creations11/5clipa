01923ed993d2e51c3ab8c3e8273f4eb0e44de7bf
Easybranding <sogulaa@gmail.com>
2026-05-13 08:54:38 +0200

    Add validation functions for SAID and phone number

--- ├── js/validation.js
+++ ├── js/validation.js
@@ -1,1 +1,15 @@
+function validateSAID(idNumber){
 
+if(idNumber.length !== 13){
+  return false;
+}
+
+return /^\d+$/.test(idNumber);
+
+}
+
+function validatePhone(phone){
+
+return phone.length >= 10;
+
+}
