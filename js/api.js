8c1a5f4ac6626583e2a405b6f6beb088176806ef
Easybranding <sogulaa@gmail.com>
2026-05-13 15:50:34 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -5,37 +5,39 @@ async function submitApplication(data){
 
 try{
 
+const formData =
+new FormData();
+
+for(const key in data){
+
+formData.append(
+key,
+data[key]
+);
+
+}
+
 const response =
 await fetch(API_URL,{
 
 method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify(data)
+body:formData
 
 });
 
 const text =
 await response.text();
 
-console.log("RAW RESPONSE:", text);
+console.log(text);
 
 const result =
 JSON.parse(text);
-
-console.log("PARSED RESPONSE:", result);
 
 return result;
 
 }catch(error){
 
-console.error(
-"API ERROR:",
-error
-);
+console.error(error);
 
 return {
 success:false
